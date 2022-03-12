8f591cdf117aec4eed70bfdd3e8e7983ddfea7cd
gpay <gpaytakov@gmail.com>
2022-03-12 11:41:37 -0600

    current date added

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+var importantEvents = {};
+var showCurrentDay = $('#currentDay')
+var today = moment().format('dddd MMMM Do');
+
+// shows current day on header
+showCurrentDay.text(today);
+
+// var pastEvent = 
+// var currentEvent =
+// var futureEvent = 
+
+// var createEvent = function (eventText, eventDate, eventList) {
+//     // create elemenets that make up a event item
+//     var eventLi = $
+// }
