3bc2d1599be4cf950fdb0fb814830f9bd9089a75
gpay <gpaytakov@gmail.com>
2022-04-23 00:40:22 -0500

    create elements in js, use functions to make code maintanable,  refactor code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,58 +1,76 @@
-var importantEvents = {};
 var showCurrentDay = $('#currentDay')
 var today = moment().format('dddd, MMMM Do');
+const startTime = 8; //8AM
+const endTime = 17; //5PM
 
 // shows current day on header
 showCurrentDay.text(today);
 
-$('.saveBtn').on('click', function() {
-    var text = $(this).siblings('.description').val();
-    var time = $(this).parent().attr('id');
-    
-    localStorage.setItem(time, text);
-});
+const createTimeBlock = (time) => {
+    const timeInAMorPM = time % 12 || 12;
+    const timeUnit = (time >= 12) ? 'PM': 'AM';
+    const divElement = document.createElement('div');
+    divElement.className = 'row time-block';
+    divElement.id = `hour${time}`;
+    const hourLabel =document.createElement('div');
+    hourLabel.className = 'col-sm-1 hour';
+    hourLabel.innerText = `${timeInAMorPM} ${timeUnit}`;
+    const textArea = document.createElement('textarea');
+    textArea.className = 'col-sm-10 description';
+    const btn = document.createElement('button');
+    btn.className = 'col-sm-1 btn saveBtn';
+    const icon = document.createElement('i');
+    icon.className = 'fas fa-save';
+    btn.appendChild(icon);
+    [hourLabel, textArea, btn].forEach(element => {
+        divElement.appendChild(element);
+    });
+    return divElement;
+} 
 
-$('#hour8 .description').val(localStorage.getItem('hour8'));
-$('#hour9 .description').val(localStorage.getItem('hour9'));
-$('#hour10 .description').val(localStorage.getItem('hour10'));
-$('#hour11 .description').val(localStorage.getItem('hour11'));
-$('#hour12 .description').val(localStorage.getItem('hour12'));
-$('#hour13 .description').val(localStorage.getItem('hour13'));
-$('#hour14 .description').val(localStorage.getItem('hour14'));
-$('#hour15 .description').val(localStorage.getItem('hour15'));
-$('#hour16 .description').val(localStorage.getItem('hour16'));
+const generateTimeBlocks = () => {
+    const container = $('.container');
+    for (let i=startTime; i<endTime; i++) {
+        const timeBlock = createTimeBlock(i);
+        container.append(timeBlock);
+    }
+
+}
+
+const getSavedTimeBlockInfo = () => {
+    for (let i=startTime; i<endTime; i++) {
+        $(`#hour${i} .description`).val(localStorage.getItem(`hour${i}`));
+    }
+}
 
 function hourCheck() {
     var currentHour = moment().hour(); // get current hour using moment.js
 
-    // time-blocks looped to set status
-    $('.time-block').each(function() {
+    $('.time-block').toggleClass(function() {
         var hourOfBlock = parseInt($(this).attr('id').split('hour')[1]);
         console.log(hourOfBlock, currentHour)
 
         if (hourOfBlock < currentHour) {
-            $(this).addClass('past');
-            $(this).removeClass('present');
-            $(this).removeClass('future');
+            return $(this).addClass('past');           
         } else if (hourOfBlock === currentHour) {
-            $(this).removeClass('past');
-            $(this).addClass('present');
-            $(this).removeClass('future');
+            return $(this).addClass('present');
         } else {
-            $(this).removeClass('past');
-            $(this).removeClass('present');
-            $(this).addClass('future');
+            return $(this).addClass('future');
         }
     })
 }
 
-hourCheck();
+const main = () => {
+    generateTimeBlocks();
+    getSavedTimeBlockInfo();
+    hourCheck();
+    $('.saveBtn').on('click', function() {
+        var text = $(this).siblings('.description').val();
+        var time = $(this).parent().attr('id');
+        
+        localStorage.setItem(time, text);
+        console.log(time, text);
+    });
+}
 
-// var pastEvent = 
-// var currentEvent =
-// var futureEvent = 
-
-// var createEvent = function (eventText, eventDate, eventList) {
-//     // create elemenets that make up a event item
-//     var eventLi = $
-// }
+main();
